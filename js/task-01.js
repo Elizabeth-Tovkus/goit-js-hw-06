cb4ca7a76b08a7488cf55c7e79ac1e688f64bcb3
Elizabeth-Tovkus <eliza.tovkus@gmail.com>
2022-10-28 22:55:34 +0300

    try №1

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+const item = document.querySelectorAll('li.item');
+const categories = document.querySelector('#categories');
+console.log(item);
+// console.log(categories.textContent);
+
+// const h2 = document.querySelector('h2');
+// console.log('Category:', h2.textContent);
+
+// for (let i = 0; i < categories.length; i += 1){
+//     console.log(`Ind ${i}, value ${item[i]}`)
+// };
---
+++ js/task-02.js
@@ -0,0 +1,11 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+const ul = document.querySelector('#ingredients');
+// const markup = ingredients.map(ingredients => )
